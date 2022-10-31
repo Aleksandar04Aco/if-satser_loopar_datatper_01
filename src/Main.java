563abe587cae3a2b47ca7d68866d52f86b459d42
Aco <92083072+Aleksandar04Aco@users.noreply.github.com>
2022-10-31 10:40:31 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.security.SignedObject;
+import java.sql.SQLOutput;
+import java.util.Date;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //uppg1();
+        uppg2();
+    }
+
+    public static void uppg1(){
+        if (6>=3){
+            System.out.println("Hello World!");
+        }
+}
+
+    public static void uppg2(){
+
+        Scanner name = new Scanner(System.in);
+        System.out.println("Enter username");
+
+        String userName = name.nextLine();
+        System.out.println("Username is: " + userName);
+
+        if (userName.equals("noname")) {
+            System.out.println("Welcome!");
+        }
+            System.out.println(userName.compareTo("noname"));
+            System.out.println("noname ==" + userName);
+    }
+}
